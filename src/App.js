4fed150711dfb91d26b246146109a9918f34acd7
karimhantaou <karim.hantaou@gmail.com>
2025-07-17 20:11:13 +0200

    Add : pfp et social links
    Ajout de ma photo
    Des liens qui redirige vers la section me contacter
    
    Améliorer l'organisation du CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+// Import des dépendances
 import { useEffect, useRef, useState } from 'react';
+import {FaGithub, FaLinkedin} from "react-icons/fa";
+import {CiMail} from "react-icons/ci";
 
+// Import des styles
 import './style/Main.css';
 import './style/NavBar.css';
 
+
+// Import des composants
 import VerticalLine from "./components/VerticalLine";
 import NavBar from "./components/NavBar";
+
+import {FaLetterboxd} from "react-icons/fa6";
 
 function App() {
     const navRef = useRef(null);
@@ -37,16 +45,25 @@ function App() {
             {/* Affiche NavBar seulement après la section skills */}
 
             {showNavBar && <NavBar />}
+            <img src="/images/photo.png" className={"pfp"}/>
 
             {/* Section d'intro */}
-            <div className="section" id="presentation" ref={navRef}>
+            <div className="section" id="presentation">
                 {/* Partie de gauche */}
+
                 <div className="left-side">
+
                     <p className="name">Hantaou Karim</p>
                     <p className="title">Développeur Full Stack</p>
 
                     <a download="CV Hantaou Karim.pdf" href="/pdfs/CV.pdf" className="button-cv">
                         Télécharger mon CV
+                    </a>
+
+                    <a className="social-links">
+                        <a href="#contact"><FaGithub size={40}/></a>
+                        <a href="#contact"><FaLinkedin size={40}/></a>
+                        <a href="#contact"><CiMail size={40}/></a>
                     </a>
 
                     <p className="presentation">
@@ -65,11 +82,11 @@ function App() {
                     <div className="right-side-content">
                         <VerticalLine height="100%" width="2" color="var(--accent-color)" />
                         <div className="links">
-                            <a href="#presentation">Ma présentation</a>
+                            <a href="#presentation">A propos de moi</a>
                             <a href="#skills">Mes compétences</a>
                             <a href="#projects">Mes projets</a>
                             <a href="#career">Mon parcours</a>
-                            <a href="#contact">Me contacter</a>
+                            <a href="#contact" ref={navRef}>Me contacter</a>
                         </div>
                     </div>
                 </div>
