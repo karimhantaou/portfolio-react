8b19a00e30d793eb7b29ba06c59d94759428fec7
karimhantaou <karim.hantaou@gmail.com>
2025-08-19 17:31:14 +0200

    Update : Resolution de redirection

--- src/components/ResolutionWarning.js
+++ src/components/ResolutionWarning.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 
 function ResolutionWarning() {
-    const [showWarning, setShowWarning] = useState(false);
-
     useEffect(() => {
         const checkResolution = () => {
             const width = window.innerWidth;
@@ -10,11 +8,9 @@ function ResolutionWarning() {
 
             console.log(`Current resolution: ${width}x${height}`);
 
-            if (width < 1280 || height < 720) {
+            if (width < 1150 || height < 620) {
                 window.location.href = "https://hantaoukarim.fr/mobile/";
-            } else {
-                setShowWarning(false);
-            }
+                }
         };
 
         checkResolution(); // Vérifie au chargement
@@ -22,29 +18,6 @@ function ResolutionWarning() {
 
         return () => window.removeEventListener('resize', checkResolution);
     }, []);
-
-    if (!showWarning) return null;
-
-    return (
-        <div style={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            background: 'var(--primary-color)',
-            color: 'var(--secondary-color)',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            zIndex: 9999999
-        }}>
-            <div style={{ textAlign: 'center', maxWidth: '90%' }}>
-                <h2>💡 Taille de l'écran trop petit</h2>
-                <p>Ce site n'est pas optimisé pour les mobiles !</p>
-            </div>
-        </div>
-    );
 }
 
 export default ResolutionWarning;
